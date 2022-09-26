b0ca4b68f15495eadadddb963d2f6ba0e35231ed
andAndrew <andAndrew.k@gmail.com>
2022-09-26 20:30:25 +0400

    feat: added text and photo blocks in Main

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -3,7 +3,14 @@ import styles from './Main.module.css'
 export const Main = () => {
     return (
         <div className={styles.mainBlock}>
-
+            <div className={styles.container}>
+                <div className={styles.greetingsText}>
+                    <span>Hi There</span>
+                    <h1>I am Krivokhizhin Andrey</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={styles.photo}></div>
+            </div>
         </div>
     )
 }
