908e5f0f688438dd30955c91092a5e92797accf0
andandrew <andrew.krivokhizhin@gmail.com>
2022-12-04 19:48:56 +0400

    feat: finallied design of Contacts block

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,15 +1,16 @@
-import styles from './Contacts.module.css'
+import styles from './Contacts.module.scss'
 import styleContainer from "../Common/Styles/Container.module.css";
+import {Title} from "../Common/Components/Title/Title";
 
 export const Contacts = () => {
     return (
         <div className={styles.contactsBlock}>
             <div className={`${styleContainer.container} ${styles.contactsContainer}`}>
-                <h2 className={styles.title}>Contacts</h2>
+                <Title title={'Contact'}/>
                 <form className={styles.contactsForm}>
-                    <input className={styles.input} type="text"/>
-                    <input className={styles.input} type="text"/>
-                    <textarea className={styles.textArea}></textarea>
+                    <input className={styles.input} type="text" placeholder={'company'}/>
+                    <input className={styles.input} type="text" placeholder={'name'}/>
+                    <textarea className={styles.textArea} placeholder={'message text'}></textarea>
                 </form>
                 <a href={''} className={styles.sendButton}>Send</a>
             </div>
