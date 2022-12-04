8224ace8dd12736f824e8dee17353ef321222d8f
andandrew <andrew.krivokhizhin@gmail.com>
2022-12-04 18:15:21 +0400

    feat: added fontawesome, added Skills icons

--- src/Skills/Skill/Skill.tsx
+++ src/Skills/Skill/Skill.tsx
@@ -1,6 +1,9 @@
 import styles from './Skill.module.scss'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 type PropsType = {
+    svg?: any
+    icon?: any,
     title: string,
     description: string
 }
@@ -8,7 +11,9 @@ type PropsType = {
 export const Skill = (props: PropsType) => {
     return (
         <div className={styles.skill}>
-            <div className={styles.icon}></div>
+            {props.svg
+                ? <img src={props.svg} className={styles.icon}/>
+                : <FontAwesomeIcon className={styles.icon} icon={props.icon}/>}
             <div className={styles.skillInfo}>
                 <h3 className={styles.title}>{props.title}</h3>
                 <span className={styles.description}>{props.description}</span>
--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -2,6 +2,11 @@ import styles from './Skills.module.scss'
 import styleContainer from '../Common/Styles/Container.module.css'
 import {Skill} from "./Skill/Skill";
 import {Title} from "../Common/Components/Title/Title";
+import {faJs} from "@fortawesome/free-brands-svg-icons/faJs";
+import {faHtml5} from "@fortawesome/free-brands-svg-icons/faHtml5";
+import {faCss3} from "@fortawesome/free-brands-svg-icons/faCss3";
+import {faReact} from "@fortawesome/free-brands-svg-icons/faReact";
+import reduxIcon from "../Assets/Images/redux.svg"
 
 export const Skills = () => {
     return (
@@ -9,12 +14,21 @@ export const Skills = () => {
             <div className={`${styleContainer.container} ${styles.skillsContainer}`}>
                 <Title title={'Skills'}/>
                 <div className={styles.skills}>
-                    <Skill title={'JS'} description={'JS skills JS skills JS skills JS skills JS skills'}/>
-                    <Skill title={'HTML'} description={'HTML skills HTML skills HTML skills HTML skills HTML skills'}/>
-                    <Skill title={'CSS'}
-                           description={'CSS skills CSS skills CSS skills CSS skills CSS skills CSS skills'}/>
-                    <Skill title={'REACT'}
-                           description={'REACT skills REACT skills REACT skills REACT skills REACT skills REACT skills'}/>
+                    <Skill icon={faJs}
+                           title={'JS'}
+                           description={'JS skills JS skills'}/>
+                    <Skill icon={faHtml5}
+                           title={'HTML'}
+                           description={'HTML skills HTML skills'}/>
+                    <Skill icon={faCss3}
+                           title={'CSS'}
+                           description={'CSS skills CSS skills'}/>
+                    <Skill icon={faReact}
+                           title={'REACT'}
+                           description={'REACT skills REACT skills'}/>
+                    <Skill svg={reduxIcon}
+                           title={'REDUX'}
+                           description={'REDUX skills REDUX skills'}/>
                 </div>
             </div>
         </div>
