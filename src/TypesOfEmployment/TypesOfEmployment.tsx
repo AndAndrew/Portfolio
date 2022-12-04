7e4efdc00d06af2a3fecb22216fe8e232fcfe975
andandrew <andrew.krivokhizhin@gmail.com>
2022-12-04 18:41:57 +0400

    feat: finallied design of Types of employment component

--- src/TypesOfEmployment/TypesOfEmployment.tsx
+++ src/TypesOfEmployment/TypesOfEmployment.tsx
@@ -1,11 +1,12 @@
-import styles from './TypesOfEmployment.module.css'
+import styles from './TypesOfEmployment.module.scss'
 import styleContainer from '../Common/Styles/Container.module.css'
+import {Title} from "../Common/Components/Title/Title";
 
 export const TypesOfEmployment = () => {
     return (
         <div className={styles.TypesOfEmploymentBlock}>
             <div className={`${styleContainer.container} ${styles.typesOfEmploymentContainer}`}>
-                <h2 className={styles.title}>Considered Types Of Employment</h2>
+                <Title title={'Considered Types Of Employment'}/>
                 <div className={styles.items}>
                     <div className={styles.item}>Remote Job</div>
                     <div className={styles.item}>Full-time</div>
